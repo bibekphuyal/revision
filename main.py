77a77fa9ac3dc72c05d69b4252c893d4b406ec8e
bibek <mailbibek07@gmail.com>
2024-01-20 14:34:05 +1100

    This commit will add print statement

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+#to find the fibionacci series
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print("Hello world")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
